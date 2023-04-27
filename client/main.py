06ae406742fa10b6e6be4d96dbb2c438653976ee
soroosh <sorooshm78@gmail.com>
2023-04-27 19:02:25 +0330

    remove logging

--- client/main.py
+++ client/main.py
@@ -1,12 +1,11 @@
 import os
-import logging
 import requests
 import time
 import pyscreenshot as ImageGrab
 from datetime import datetime
 
-
 import secret
+
 
 USERNAME = secret.USERNAME
 PASSWORD = secret.PASSWORD
@@ -18,14 +17,8 @@ SHOT_TIME = secret.SHOT_TIME
 MEDIA_FOLDER_NAME = "media"
 
 
-logging.basicConfig(filename="screenshot.log")
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-
 def create_meida_folder():
     if not os.path.exists(MEDIA_FOLDER_NAME):
-        logging.info("created media folder")
         os.makedirs(MEDIA_FOLDER_NAME)
 
 
@@ -33,13 +26,12 @@ def take_screenshot(filename):
     create_meida_folder()
     screenshot = ImageGrab.grab()
     screenshot.save(f"{MEDIA_FOLDER_NAME}/{filename}.png")
-    logging.info("take screenshot")
 
 
 def login_user(session):
     res = session.get(LOGIN_URL)
     csrf_token = res.cookies["csrftoken"]
-    res = session.post(
+    session.post(
         LOGIN_URL,
         data={
             "username": USERNAME,
@@ -47,10 +39,6 @@ def login_user(session):
             "csrfmiddlewaretoken": csrf_token,
         },
     )
-    if res.status_code == 200:
-        logging.info("User login successful")
-    else:
-        logging.error("user can not login")
 
 
 def send_screenshot_to_server(session, filename, created):
@@ -62,11 +50,7 @@ def send_screenshot_to_server(session, filename, created):
         "created": created.isoformat(),
     }
 
-    res = session.post(UPLOAD_URL, files=files, data=data)
-    if res.status_code == 200:
-        logging.info("sended screenshot to server")
-    else:
-        logging.error("screenshot not sended to server")
+    session.post(UPLOAD_URL, files=files, data=data)
 
 
 with requests.Session() as session:
