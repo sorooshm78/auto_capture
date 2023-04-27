d21ea6a7d1d10aea37e6e26a70e243bbc43f5853
soroosh <sorooshm78@gmail.com>
2023-04-27 16:13:44 +0330

    add account app

---
+++ server/account/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import CreateView
+from django.urls import reverse
+
+from .forms import RegisterModelForm
+
+
+# Create your views here.
+class UserRegistrationView(CreateView):
+    form_class = RegisterModelForm
+    template_name = "registration/register.html"
+
+    def get_success_url(self):
+        return reverse("account:login")
