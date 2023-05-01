082f4b3d6cec525974f7fe3b7997b20cc93759e5
soroosh <sorooshm78@gmail.com>
2023-05-01 16:01:28 +0330

    add bootstrap

--- server/core/settings.py
+++ server/core/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
